d4db6cd8c142b94cf94ac28ef7c6879ceed5f7c9
John Morton <john@johnfmorton.com>
2015-08-10 16:35:54 -0400

    moved files out of subfolder to top of directory
    Signed-off-by: John Morton <john@johnfmorton.com>

---
+++ generators/app/templates/dev/script.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+// HTML5 Ad Template JS from DoubleClick by Google
+
+//Declaring elements from the HTML i.e. Giving them Instance Names like in Flash - makes it easier
+var container;
+var content;
+var bgExit;
+
+// var someElementInHTML;
+
+//Function to run with any animations starting on load, or bringing in images etc
+// bannerInit is
+bannerInit = function(){
+
+    //Assign All the elements to the element on the page
+    container = document.getElementById('container_dc');
+    content = document.getElementById('content_dc');
+    bgExit = document.getElementById('background_exit_dc');
+    // someElementInHTML = document.getElementById('some_element_in_html');
+
+    //Setup Background Image (this can be done in CSS as well
+    // which it is in the default template.)
+    //
+    //content.style.backgroundImage = "url("+Enabler.getUrl('background.jpg')+")";
+
+    //Bring in listeners i.e. if a user clicks or rollovers
+    addListeners();
+
+    // Show Ad (this can also be done in the index.html
+    // which it is in the default template.)
+    //
+    // container.style.display = "block";
+
+    anim();
+}
+
+anim = function(){
+    console.log('Banner animation has begun.');
+
+    // Example #1
+    //
+    // If you're using TimelineLite, you can follow this example
+    // var tl = new TimelineLite();
+    // tl.to(someElementInHTML, 1, {css:{top: "100px", left: "100px"}})
+    // tl.play();
+
+    // Example #2
+    //
+    // If you're just doing TweenLite directly without TimelineLite
+    // TweenLite.to(someElementInHTML, 1, {css:{top: "100px", left: "100px"}});
+}
+
+//Add Event Listeners
+addListeners = function() {
+    bgExit.addEventListener('click', bgExitHandler, false);
+}
+
+bgExitHandler = function(e) {
+    //Call Exits
+    Enabler.exit('HTML5_Background_Clickthrough');
+}
