1cc6f4aa15345e4e1f67fa48db1d70f1262f518a
John Morton <john@johnfmorton.com>
2015-08-23 08:56:22 -0400

    Removed unintendedly included animation code
    This was animation code from an actual banner. It made no sense in this template.
    
    Signed-off-by: John Morton <john@johnfmorton.com>

--- generators/app/templates/dev/_script_standard.js
+++ generators/app/templates/dev/_script_standard.js
@@ -33,13 +33,6 @@ anim = function(){
     var backSpeed = 1.2;
     var tl = new TimelineLite();
 
-    tl.to(bar, 0.25, {
-        css: {
-            top: "199px"
-        },
-        delay: 0.2,
-        ease: Back.easeOut.config(backSpeed)
-    });
     tl.to(hd1, 0.25, {
         css: {
             left: "52px"
@@ -48,72 +41,6 @@ anim = function(){
         ease: Back.easeOut.config(backSpeed)
     }, '-=0.25');
 
-    tl.to(bar, 0.25, {
-        css: {
-            top: "376px"
-        },
-        delay: 0.2,
-        ease: Cubic.easeInOut
-    }, "+=1.5");
-
-    tl.set(bar, {
-        rotation: 0,
-        top: "90px",
-        left: "-323px"
-    });
-
-    tl.to(hd1, 0.25, {
-        css: {
-            left: "-220px"
-        },
-        delay: 0.25,
-        ease: Circ.easeOut
-    }, '-=0.1');
-
-    tl.to(bar, 0.25, {
-        left: "23px",
-        ease: Back.easeOut.config(backSpeed)
-    }, "-=0.15");
-
-    tl.to(hd2, 0.25, {
-        css: {
-            left: "28px"
-        },
-        delay: 0,
-        ease: Back.easeOut.config(backSpeed)
-    }, '+=0.25');
-
-    tl.to(hd2, 0.25, {
-        css: {
-            left: "-300px"
-        },
-        delay: 0,
-        ease: Circ.easeOut
-    }, '+=1.5');
-
-    tl.to(hd3, 0.25, {
-        css: {
-            left: "29px"
-        },
-        delay: 0,
-        ease: Back.easeOut.config(backSpeed)
-    }, '-=0.1');
-
-    tl.to(cta, 0.25, {
-        css: {
-            opacity: "1"
-        },
-        delay: 0,
-        ease: Linear.easeNone
-    }, '-=0.0');
-
-    tl.to(cta, 0.25, {
-        css: {
-            top: "178px"
-        },
-        delay: 0,
-        ease: Back.easeOut.config(backSpeed)
-    }, '-=0.25');
 
     tl.play();
 }
